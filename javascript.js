a05231b8f294ec80a0a5ea05d7d285ba242ed6b5
Matias Salinas <matixxdpls@gmail.com>
2022-09-25 22:52:44 -0300

    Added basic funtions and linked the html

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b ;
+}
+
+function division(a,b){
+    return a / b;
+}
+
+function exponent(a,b){
+    return a ** b;
+}
+
+console.log(exponent(4,2));
