c7cca7e158a296971a23d2f5cc51598b31c54741
Matias Salinas <matixxdpls@gmail.com>
2022-09-26 23:33:05 -0300

    Added operands functionality

--- javascript.js
+++ javascript.js
@@ -1,15 +1,107 @@
 const nums = document.querySelectorAll('.numButtons');
 const displayText = document.querySelector('.displayText');
-console.log(nums)
+
+let first,second,total,operator = 0;
+
 nums.forEach(num => {
     num.addEventListener('click', ()=> {
-        displayText.textContent += num.textContent;
+        if(displayText.textContent == 0 || displayText.textContent == operator){
+            displayText.textContent = num.textContent;
+        }else{
+        displayText.textContent += num.textContent;}
     })
 })
+
+const operators = document.querySelectorAll('.opButtons');
+
+operators.forEach(op => {
+    op.addEventListener('click',()=> {
+        switch (op.textContent) {
+            case '/':
+                console.log('div')
+                first = parseInt(displayText.textContent);
+                operator = '/'
+                displayText.textContent = operator;
+                break;
+
+            case 'X':
+                console.log('mult')
+                first = parseInt(displayText.textContent);
+                operator = 'X'
+                displayText.textContent = operator;
+                break;
+
+            case '-':
+                if(operator !=0){
+                    operate()
+                    displayText.textContent = total;
+                    first = total;
+                    break;
+                }
+                first = parseInt(displayText.textContent);
+                operator = '-'
+                displayText.textContent = operator;
+                break;
+
+            case '+':
+                console.log('additon')
+                first = parseInt(displayText.textContent);
+                operator = '+'
+                displayText.textContent = operator;
+                break;
+
+            case 'DEL':
+                console.log('delete')
+                displayText.textContent = displayText.textContent.slice(0,-1);
+                break;
+        
+            default:
+                break;
+        }
+    })
+})
+
+const equal = document.getElementById('equal');
+const reset = document.getElementById('reset');
+
+reset.addEventListener('click',()=> {
+    displayText.textContent =0;
+    first = 0;
+    second = 0;
+    operator = 0;
+    total = 0;
+})
+
+equal.addEventListener('click',operate);
+
+
+function operate(){
+    second = parseInt(displayText.textContent);
+    switch (operator) {
+        case '/':
+            total = division(first,second);
+            displayText.textContent= total;
+            break;
+
+        case 'X':
+            total = multiply(first,second);
+            displayText.textContent= total;
+            break;
+
+        case '-':
+            total = subtract(first,second);
+            displayText.textContent= total;
+            break;
+
+        case '+':
+            total = add(first,second);
+            displayText.textContent= total;
+            break;
     
-
-
-
+        default:
+            break;
+    }
+}
 
 
 
@@ -32,5 +124,3 @@ function division(a,b){
 function exponent(a,b){
     return a ** b;
 }
-
-console.log(exponent(4,2));
